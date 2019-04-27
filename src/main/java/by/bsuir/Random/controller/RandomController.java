c1e8af3e3c51cbb201cabf42ae3a3c833831d907
VictorShedko <48150306+VictorShedko@users.noreply.github.com>
2019-04-27 15:06:11 +0300

    Parse throw int array

--- src/main/java/by/bsuir/Random/controller/RandomController.java
+++ src/main/java/by/bsuir/Random/controller/RandomController.java
@@ -2,26 +2,96 @@ package by.bsuir.Random.controller;
 
 
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import by.bsuir.Random.entity.myRandom;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RandomController {
     myRandom random;
     @PostMapping(value="/random")
-    public List<Integer> getRandom(@RequestBody MappingInt mappingInt ) {
+    public List<Integer> getRandom(@RequestBody Integer[] jsonString ) {
         {
 
-            int test;
-            test=3;
-            return mappingInt.getItemList().stream().map(u->random.getRand(u)).collect(Collectors.toList());
+            //Gson g=new Gson();
+            //MappingInt p = g.fromJson(jsonString, MappingInt.class);
+            //JsonObject jsonObject=JSON.parse(jsonString);
+            //return p.getItemList().stream().map(u->random.getRand(u)).collect(Collectors.toList());
 
+           List<Integer> inList=new ArrayList<Integer>(Arrays.asList(jsonString));
+
+
+
+            return inList.stream().map(u->random.getRand(u)).collect(Collectors.toList());
 
         }
 
 
     }
+
+    @PostMapping(value="/random1")
+    public List<Integer> getRandomWithoutReapet(@RequestBody Integer[] jsonString ) {
+        {
+
+            //Gson g=new Gson();
+            //MappingInt p = g.fromJson(jsonString, MappingInt.class);
+            //JsonObject jsonObject=JSON.parse(jsonString);
+            //return p.getItemList().stream().map(u->random.getRand(u)).collect(Collectors.toList());
+
+            List<Integer> inList=new ArrayList<Integer>(Arrays.asList(jsonString));
+
+
+
+            return inList.stream().map(u->random.getRand(u)).distinct().collect(Collectors.toList());
+
+        }
+
+
+
+        @PostMapping(value="/random1")
+        public List<Integer> getRandomWithoutReapet(@RequestBody Integer[] jsonString ) {
+            {
+
+                //Gson g=new Gson();
+                //MappingInt p = g.fromJson(jsonString, MappingInt.class);
+                //JsonObject jsonObject=JSON.parse(jsonString);
+                //return p.getItemList().stream().map(u->random.getRand(u)).collect(Collectors.toList());
+
+                List<Integer> inList=new ArrayList<Integer>(Arrays.asList(jsonString));
+
+
+
+                return inList.stream().map(u->random.getRand(u)).distinct().collect(Collectors.toList());
+
+            }
+
+    }
+
+        @PostMapping(value="/random2")
+        public List<Integer> getRandomAt(@RequestBody Integer[] jsonString ) {
+            {
+
+                //Gson g=new Gson();
+                //MappingInt p = g.fromJson(jsonString, MappingInt.class);
+                //JsonObject jsonObject=JSON.parse(jsonString);
+                //return p.getItemList().stream().map(u->random.getRand(u)).collect(Collectors.toList());
+
+                List<Integer> inList=new ArrayList<Integer>(Arrays.asList(jsonString));
+
+
+
+                return inList.stream().map(u->random.getRand(u)).distinct().collect(Collectors.toList()).add();
+
+            }
+
 }
